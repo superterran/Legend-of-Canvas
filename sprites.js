dcf79f513e8ae737473ce7b823903767a4f0ce32
me <superterran@gmail.com>
2011-10-23 04:00:00 -0400

    refactored, added animation, colision detection, etc. still very basic, broke some things.

---
+++ sprites.js
@@ -0,0 +1,58 @@
+sprite = new Array;
+
+// Player
+
+sprite['player'] = [];
+sprite['player']['down'] = [];
+sprite['player']['down'][0] = "sprites/link-down0.png";
+sprite['player']['down'][1] = "sprites/link-down1.png";
+sprite['player']['down'][2] = "sprites/link-down2.png";
+sprite['player']['down'][3] = "sprites/link-down3.png";
+sprite['player']['down'][4] = "sprites/link-down4.png";
+sprite['player']['down'][5] = "sprites/link-down5.png";
+
+// link moving up
+
+sprite['player']['up'] = [];
+sprite['player']['up'][0] = "sprites/link-up0.png";
+sprite['player']['up'][1] = "sprites/link-up1.png";
+sprite['player']['up'][2] = "sprites/link-up2.png";
+sprite['player']['up'][3] = "sprites/link-up3.png";
+sprite['player']['up'][4] = "sprites/link-up4.png";
+sprite['player']['up'][5] = "sprites/link-up5.png";
+
+// link left
+
+sprite['player']['left'] = [];
+sprite['player']['left'][0] = "sprites/link-left0.png";
+sprite['player']['left'][1] = "sprites/link-left1.png";
+sprite['player']['left'][2] = "sprites/link-left2.png";
+sprite['player']['left'][3] = "sprites/link-left3.png";
+sprite['player']['left'][4] = "sprites/link-left4.png";
+sprite['player']['left'][5] = "sprites/link-left5.png";
+
+// link right
+
+sprite['player']['right'] = [];
+sprite['player']['right'][0] = "sprites/link-right0.png";
+sprite['player']['right'][1] = "sprites/link-right1.png";
+sprite['player']['right'][2] = "sprites/link-right2.png";
+sprite['player']['right'][3] = "sprites/link-right3.png";
+sprite['player']['right'][4] = "sprites/link-right4.png";
+sprite['player']['right'][5] = "sprites/link-right5.png";
+
+
+sprite['player']['orientation'] = 'down';
+sprite['player']['step'] = 0;
+
+
+
+// blue hard hat
+
+sprite['bluehardhat'] = [];
+sprite['bluehardhat']['down'] = [];
+sprite['bluehardhat']['down'][0] = "sprites/hardhatblue0.png";
+sprite['bluehardhat']['down'][1] = "sprites/hardhatblue1.png";
+sprite['bluehardhat']['down'][2] = "sprites/hardhatblue2.png";
+sprite['bluehardhat']['orientation'] = 'down';
+sprite['bluehardhat']['step'] = 0;
