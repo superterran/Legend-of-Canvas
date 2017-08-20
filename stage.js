fb2c9c556a5212fd84283d663d61bc26724cae37
Doug Hatcher <superterran@gmail.com>
2017-08-20 00:39:55 -0400

    Update stage.js

--- stage.js
+++ stage.js
@@ -28,68 +28,68 @@ var stage = Class.create({
         // this.level = this.parent.loadJson(this.levelsPath+name+"/level.js");
         
         this.level = {
-    "name": "dungeon",
+        "name": "dungeon",
 
-    "tiles":
-    {
-        " ":    "tile-floor.jpg",
-        "r":    "tile-wall.jpg",
-        "g":    "firepit.png"
-    },
+        "tiles":
+        {
+            " ":    "tile-floor.jpg",
+            "r":    "tile-wall.jpg",
+            "g":    "firepit.png"
+        },
 
-    "levels":
-    {
-        "0":[
-            "rrrrrrrrrrrrr",
-            "r           r",
-            "r  g     g  r",
-            "r           r",
-            "3     g     r",
-            "r           r",
-            "r  g  0  g  r",
-            "r           r",
-            "rrrrrr1rrrrrr"
-        ],
+        "levels":
+        {
+            "0":[
+                "rrrrrrrrrrrrr",
+                "r           r",
+                "r  g     g  r",
+                "r           r",
+                "3     g     r",
+                "r           r",
+                "r  g  0  g  r",
+                "r           r",
+                "rrrrrr1rrrrrr"
+            ],
 
-        "1":[
-            "rrrrrr0rrrrrr",
-            "r           r",
-            "r     g     r",
-            "r           r",
-            "2  g     g  r",
-            "r           r",
-            "r     g     r",
-            "r           r",
-            "rrrrrrrrrrrrr"
-        ],
+            "1":[
+                "rrrrrr0rrrrrr",
+                "r           r",
+                "r     g     r",
+                "r           r",
+                "2  g     g  r",
+                "r           r",
+                "r     g     r",
+                "r           r",
+                "rrrrrrrrrrrrr"
+            ],
 
-        "2":[
-            "rrrrrr3rrrrrr",
-            "r           r",
-            "r  g     g  r",
-            "r   g       r",
-            "r    g      1",
-            "r     g     r",
-            "r  g   ggg  r",
-            "r           r",
-            "rrrrrrrrrrrrr"
-        ],
+            "2":[
+                "rrrrrr3rrrrrr",
+                "r           r",
+                "r  g     g  r",
+                "r   g       r",
+                "r    g      1",
+                "r     g     r",
+                "r  g   ggg  r",
+                "r           r",
+                "rrrrrrrrrrrrr"
+            ],
 
-        "3":[
-            "rrrrrrrrrrrrr",
-            "r           r",
-            "r  g     g  r",
-            "r           r",
-            "r           0",
-            "r           r",
-            "r  g        r",
-            "r           r",
-            "rrrrrr2rrrrrr"
-        ]
+            "3":[
+                "rrrrrrrrrrrrr",
+                "r           r",
+                "r  g     g  r",
+                "r           r",
+                "r           0",
+                "r           r",
+                "r  g        r",
+                "r           r",
+                "rrrrrr2rrrrrr"
+            ]
 
 
-    };
-        
+        }
+            
         this.room = 0;
         this.lastRoom = 0;
         this.renderStage();
