03057a14076ad18905926573430c8473cc15025a
Doug Hatcher <superterran@gmail.com>
2017-08-21 01:17:43 -0400

    Update level.js

--- levels/dungeon/level.js
+++ levels/dungeon/level.js
@@ -1,1 +1,68 @@
-This is a test
+var stage = Class.create({
+level:     
+    
+{
+    "name": "dungeon",
+
+    "tiles":
+    {
+        " ":    "tile-floor.jpg",
+        "r":    "tile-wall.jpg",
+        "g":    "firepit.png"
+    },
+
+    "levels":
+    {
+        "0":[
+            "rrrrrrrrrrrrr",
+            "r           r",
+            "r  g     g  r",
+            "r           r",
+            "3     g     r",
+            "r           r",
+            "r  g  0  g  r",
+            "r           r",
+            "rrrrrr1rrrrrr"
+        ],
+
+        "1":[
+            "rrrrrr0rrrrrr",
+            "r           r",
+            "r     g     r",
+            "r           r",
+            "2  g     g  r",
+            "r           r",
+            "r     g     r",
+            "r           r",
+            "rrrrrrrrrrrrr"
+        ],
+
+        "2":[
+            "rrrrrr3rrrrrr",
+            "r           r",
+            "r  g     g  r",
+            "r   g       r",
+            "r    g      1",
+            "r     g     r",
+            "r  g   ggg  r",
+            "r           r",
+            "rrrrrrrrrrrrr"
+        ],
+
+        "3":[
+            "rrrrrrrrrrrrr",
+            "r           r",
+            "r  g     g  r",
+            "r           r",
+            "r           0",
+            "r           r",
+            "r  g        r",
+            "r           r",
+            "rrrrrr2rrrrrr"
+        ]
+
+
+    }
+}
+
+});
